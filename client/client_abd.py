4484c64fed2c0a0b783706129058209adc1c0085
60302700 <60302700@udst.edu.qa>
2025-11-21 15:56:29 +0300

    Client Side BareBones

--- client/client_abd.py
+++ client/client_abd.py
@@ -6,17 +6,24 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import hashes
-from prompt_toolkit import PromptSession
+from pick import pick
+import os
 
-session = PromptSession()
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def login():
+    clear()
+    username = input("Username: ")
+    password = input("Password: ")
+def register():pass
+def getcredentials():pass
+def quit():
+    clear()
+    exit()
+options = {"Login":login,"Register":register,"Get Credentials":getcredentials,"Exit":quit}
+title = "CryptoLogin"
 
 while True:
-    try:
-        text = session.prompt("auth> ")
-        if text.strip() == "exit":
-            break
-        print(f"You typed: {text}")
-    except KeyboardInterrupt:
-        continue
-    except EOFError:
-        break
+    option = pick(list(options.keys()),title)
+    options.get(option[0])()
